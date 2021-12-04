6f9274ca9af00d38396afd4573d34f9f4c490c89
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-12-05 00:26:25 +0200

    Add angular http client

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 import { Task } from '../Task';
 import { TASKS } from '../mock-tasks';
-import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
