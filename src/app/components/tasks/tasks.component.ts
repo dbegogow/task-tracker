fcd8aba7f33a7a1e172c8a0f2863517bd28209c2
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-12-07 17:11:16 +0200

    Implement deleting tasks

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -18,4 +18,11 @@ export class TasksComponent implements OnInit {
       .subscribe((tasks) =>
         this.tasks = tasks);
   }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id)))
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +13,10 @@ export class TaskService {
 
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
   }
 }
